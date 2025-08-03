f3e7bbd220b9205b643d63266d0351fb3d906b2d
Angel Raymondi <me@angelrd.com>
2025-08-02 20:10:33 -0500

    workaround: uncaught ReferenceError: MessageChannel is not defined

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,31 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from '@astrojs/react'
+import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      ssr: {
-          noExternal: ["@subframe/core"],
-      },
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  api: "modern-compiler",
-              },
-          },
-      },
+	vite: {
+		ssr: {
+			noExternal: ["@subframe/core"],
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: "modern-compiler",
+				},
+			},
+		},
+		resolve: {
+			alias: import.meta.env.PROD
+				? {
+						"react-dom/server": "react-dom/server.edge",
+				  }
+				: undefined,
+		},
 	},
 
-  integrations: [react()],
-  adapter: cloudflare(),
+	integrations: [react()],
+	adapter: cloudflare(),
 });
